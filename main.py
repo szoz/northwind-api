00c34998f15087bc1224832bc2f95872f8537e8d
Paweł Tymosiak <szozr2@gmail.com>
2021-05-08 10:17:27 +0200

    Added categories and customers endpoint Exercise 4.1 - added endpoints categories and customers with tests

--- main.py
+++ main.py
@@ -29,9 +29,35 @@ async def index():
 @app.get('/products')
 async def get_products():
     """Return list of all Products and their count."""
-    cursor = app.db_connection.cursor()
-    products = [row[0] for row in cursor.execute("SELECT ProductName FROM Products").fetchall()]
-    return {
-        "products": products,
-        "products_counter": len(products)
-    }
+    products = [row[0] for row in app.db_connection.execute("SELECT ProductName FROM Products").fetchall()]
+    payload = {"products": products, "products_counter": len(products)}
+
+    return payload
+
+
+@app.get('/categories')
+async def get_categories():
+    """Return list all Categories with ids."""
+    query = "SELECT CategoryID, CategoryName FROM Categories"
+    records = app.db_connection.execute(query).fetchall()
+    categories = [{'id': record[0], 'name': record[1]}
+                  for record in records]
+    payload = {'categories': categories}
+
+    return payload
+
+
+@app.get('/customers')
+async def get_customers():
+    """Return list all customers with details."""
+    query = "SELECT CustomerID, CompanyName, Address, PostalCode, City, Country FROM Customers"
+    records = app.db_connection.execute(query).fetchall()
+    customers = []
+    for record in records:
+        address = [item if item is not None else ''
+                   for item in record[2:]]
+        customer = {'id': record[0], 'name': record[1], 'full_address': ' '.join(address)}
+        customers.append(customer)
+    payload = {'customers': customers}
+
+    return payload
--- test_main.py
+++ test_main.py
@@ -12,11 +12,41 @@ def client():
 
 
 def test_products(client):
-    """Test '/products' endpoint with products list."""
+    """Test '/products' endpoint."""
     test_path = '/products'
 
     response = client.get(test_path)
+    payload = response.json()
 
     assert response.status_code == 200
-    assert type(response.json().get('products')) is list
-    assert len(response.json()['products']) == response.json()['products_counter']
+    assert type(payload.get('products')) is list
+    assert len(payload['products']) == payload['products_counter']
+
+
+def test_categories(client):
+    """Test '/categories' endpoint."""
+    test_path = '/categories'
+    test_category = {"id": 1, "name": "Beverages"}
+
+    response = client.get(test_path)
+    payload = response.json()
+
+    assert response.status_code == 200
+    assert type(payload) is dict
+    assert type(payload.get('categories')) is list
+    assert test_category in payload['categories']
+
+
+def test_customers(client):
+    """Test '/customers' endpoint."""
+    test_path = '/customers'
+    test_customer = {'id': 'ALFKI', 'name': 'Alfreds Futterkiste',
+                     'full_address': 'Obere Str. 57 12209 Berlin Germany'}
+
+    response = client.get(test_path)
+    payload = response.json()
+
+    assert response.status_code == 200
+    assert type(payload) is dict
+    assert type(payload.get('customers')) is list
+    assert test_customer in payload['customers']
