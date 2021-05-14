03b5e90b3bc390a8c8485895f4eea1b92cb4b66a
Paweł Tymosiak <szozr2@gmail.com>
2021-05-14 15:47:02 +0200

    Fixed database connection Added database URL fix for Heroku.

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import environ
 
-SQLALCHEMY_DATABASE_URL = environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URL = environ['DATABASE_URL'].replace("postgres://", "postgresql://", 1)  # Heroku config fix
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
